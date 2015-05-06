b2542da26acd7e36dd511a6b60f5ede3277fbc2f
maassander <sander.maas@student.ap.be>
2015-05-06 15:50:15 +0200

    GITIGNORE

---
+++ app/src/main/java/ap/plantuiandroidapp/MainActivity.java
@@ -0,0 +1,211 @@
+package ap.plantuiandroidapp;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener{
+
+    Button Timerbtn, Bluetoothbtn, bluebtn, redbtn, greenbtn, LoadValues;
+
+    SeekBar SeekbarRed ,SeekbarGreen ,SeekbarBlue;
+
+    TextView RedValue, BlueValue, GreenValue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Timerbtn = (Button) findViewById(R.id.ButtonGoToTimerMenu);
+        Timerbtn.setOnClickListener(this);
+        bluebtn = (Button)findViewById(R.id.Btn_Blue);
+        bluebtn.setOnClickListener(this);
+        Bluetoothbtn = (Button) findViewById(R.id.BtnGoToBluetooth);
+        Bluetoothbtn.setOnClickListener(this);
+        redbtn = (Button)findViewById(R.id.Btn_Red);
+        redbtn.setOnClickListener(this);
+        greenbtn = (Button)findViewById(R.id.Btn_Green);
+        greenbtn.setOnClickListener(this);
+        SeekbarRed = (SeekBar)findViewById(R.id.SeekbarRed);
+        SeekbarGreen = (SeekBar)findViewById(R.id.SeekbarGreen);
+        SeekbarBlue = (SeekBar)findViewById(R.id.SeekbarBlue);
+        SeekbarRed.setMax(255);
+        SeekbarRed.setEnabled(false);
+        LoadValues = (Button)findViewById(R.id.ButtonLoadValues);
+        LoadValues.setOnClickListener(this);
+        SeekbarRed.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                RedValue.setText(progress + "");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        SeekbarBlue.setMax(255);
+        SeekbarBlue.setEnabled(false);
+        SeekbarBlue.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                BlueValue.setText(progress + "");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        SeekbarGreen.setMax(255);
+        SeekbarGreen.setEnabled(false);
+        SeekbarGreen.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                GreenValue.setText(progress + "");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        RedValue = (TextView)findViewById(R.id.TextViewSeekBarRed);
+        BlueValue =(TextView)findViewById(R.id.TextViewSeekBarBlue);
+        GreenValue =(TextView)findViewById(R.id.TextViewSeekBarGreen);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            final AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+            alert.setMessage("Need help at main screen?\n" +
+                    "\n" +
+                    "What can you see from main screen?\n" +
+                    "\t- Visual values of lights\n" +
+                    "\n" +
+                    "What can you set up from main screen?\n" +
+                    "\t- Go to bluetooth and connect to device\n" +
+                    "\t- Go to clock screen and set sleep timings\n" +
+                    "\t- Go to led screen to adjust lights\n" +
+                    "\n" +
+                    "Still having problems?\n" +
+                    "Go to plantui.com/support/application/main\n" +
+                    "\n" +
+                    "Or send feedback or support request directly to us from\n" +
+                    "plantui.com/support");
+            alert.setTitle("Help");
+            alert.setIcon(R.drawable.help);
+            alert.create();
+            alert.show();
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.ButtonGoToTimerMenu:
+                startActivity(new Intent("ap.plantuiapp.timermenu"));
+                break;
+            case R.id.BtnGoToBluetooth:
+                startActivity(new Intent("ap.plantuiapp.bluetooth"));
+                break;
+            case R.id.Btn_Blue:
+                startActivity(new Intent("ap.plantuiapp.blueled"));
+                break;
+            case R.id.Btn_Red:
+                startActivity(new Intent("ap.plantuiapp.redled"));
+                break;
+            case R.id.Btn_Green:
+                startActivity(new Intent("ap.plantuiapp.greenled"));
+            case R.id.ButtonLoadValues:
+                LoadValues();
+                break;
+        }
+    }
+    public void LoadValues()
+    {
+
+        try {
+            FileInputStream fileRedLed =  openFileInput("ValueRed.txt");
+            FileInputStream fisGreenLed = openFileInput("ValueGreen.txt");
+            FileInputStream fisBlueLed = openFileInput("ValueBlue.txt");
+            int read;
+            //a while because otherwise its only the first byte he read and then stops.
+            /*
+            *
+            *what u see when u open a file?
+            *255
+            * what it actually contained!
+            *32 40 40 for example that's why we use (char)
+             */
+
+            while ((read = fileRedLed.read()) != -1)
+            {
+                SeekbarRed.setProgress((char)read);
+            }
+            while((read = fisGreenLed.read())!=-1)
+            {
+                SeekbarGreen.setProgress((char)read);
+            }
+            while((read = fisBlueLed.read())!=-1)
+            {
+                SeekbarBlue.setProgress((char)read);
+            }
+
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ app/src/main/java/ap/plantuiandroidapp/bluetooth.java
@@ -0,0 +1,344 @@
+package ap.plantuiandroidapp;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.UUID;
+
+
+public class bluetooth extends ActionBarActivity {
+
+    private final static UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    // Create a BroadcastReceiver for ACTION_FOUND
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            // Whenever a remote Bluetooth device is found
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Get the BluetoothDevice object from the Intent
+                BluetoothDevice bluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // Add the name and address to an array adapter to show in a ListView
+                adapter.add(bluetoothDevice.getName() + "\n"
+                        + bluetoothDevice.getAddress());
+            }
+        }
+    };
+    private BluetoothAdapter bluetoothAdapter;
+    private Switch bt_Switch;
+    private ListView bt_Listview;
+    private TextView bt_Textview;
+    private ArrayAdapter adapter;
+    private static final int ENABLE_BT_REQUEST_CODE = 1;
+    private static final int DISCOVERABLE_BT_REQUEST_CODE = 2;
+    private static final int DISCOVERABLE_DURATION = 300;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth);
+
+        bt_Textview = (TextView) findViewById(R.id.bt_textview);
+        bt_Switch = (Switch) findViewById(R.id.bt_switch);
+        bt_Switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    bt_Textview.setText("Bluetooth is currently ON");
+                    if (!bluetoothAdapter.isEnabled()) {
+                        Intent enableBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                        startActivityForResult(enableBluetoothIntent, ENABLE_BT_REQUEST_CODE);
+                    } else {
+                        bt_Textview.setText("Bluetooth is ALREADY ON");
+                    }
+                } else {
+                    bluetoothAdapter.disable();
+                    bt_Textview.setText("Bluetooth is currently OFF");
+                }
+            }
+        });
+
+        bt_Listview = (ListView) findViewById(R.id.bt_listview);
+        // ListView Item Click Listener
+        bt_Listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // ListView Clicked item value
+
+                String itemValue = (String) bt_Listview.getItemAtPosition(position);
+
+                String MAC = itemValue.substring(itemValue.length() - 17);
+                bt_Textview.setText(MAC);
+                BluetoothDevice bluetoothDevice = bluetoothAdapter.getRemoteDevice(MAC);
+
+                // Initiate a connection request in a separate thread
+                ConnectingThread t = new ConnectingThread(bluetoothDevice);
+                t.start();
+
+            }
+        });
+
+        adapter = new ArrayAdapter
+                (this,android.R.layout.simple_list_item_1);
+        bt_Listview.setAdapter(adapter);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    }
+
+    public void Connect(View view)
+    {
+        String itemValue = "0C:D2:92:6D:7C:79";
+        String MAC = itemValue.substring(itemValue.length() - 17);
+        bt_Textview.setText(MAC);
+        BluetoothDevice bluetoothDevice = bluetoothAdapter.getRemoteDevice(MAC);
+
+        // Initiate a connection request in a separate thread
+        ConnectingThread t = new ConnectingThread(bluetoothDevice);
+        t.start();
+    }
+
+    public void Scan(View view) {
+
+        adapter.clear();
+
+        if (bluetoothAdapter == null) {
+            // Device does not support Bluetooth
+            Toast.makeText(getApplicationContext(), "Oops! Your device does not support Bluetooth",
+                    Toast.LENGTH_SHORT).show();
+            bt_Switch.setChecked(false);
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "Your device has already been enabled." +
+                            "\n" + "Scanning for remote Bluetooth devices...",
+                    Toast.LENGTH_SHORT).show();
+            // To discover remote Bluetooth devices
+            discoverDevices();
+            // Make local device discoverable by other devices
+            makeDiscoverable();
+        }
+    }
+
+    /*public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == ENABLE_BT_REQUEST_CODE) {
+
+            // Bluetooth successfully enabled!
+            if (resultCode == Activity.RESULT_OK) {
+                Toast.makeText(getApplicationContext(), "Bluetooth is now enabled." +
+                                "\n" + "Scanning for remote Bluetooth devices...",
+                        Toast.LENGTH_SHORT).show();
+
+                // To discover remote Bluetooth devices
+                discoverDevices();
+
+                // Make local device discoverable by other devices
+                makeDiscoverable();
+
+                // Start a thread to create a  server socket to listen
+                // for connection request
+                ListeningThread t = new ListeningThread();
+                t.start();
+
+            } else { // RESULT_CANCELED as user refused or failed to enable Bluetooth
+                Toast.makeText(getApplicationContext(), "Bluetooth is not enabled.",
+                        Toast.LENGTH_SHORT).show();
+
+                // Turn off switch
+                bt_Switch.setChecked(false);
+            }
+        } else if (requestCode == DISCOVERABLE_BT_REQUEST_CODE){
+
+            if (resultCode == DISCOVERABLE_DURATION){
+                Toast.makeText(getApplicationContext(), "Your device is now discoverable by other devices for " +
+                                DISCOVERABLE_DURATION + " seconds",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Fail to enable discoverability on your device.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }*/
+
+    protected void discoverDevices(){
+        // To scan for remote Bluetooth devices
+        if (bluetoothAdapter.startDiscovery()) {
+            Toast.makeText(getApplicationContext(), "Discovering other bluetooth devices...",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Discovery failed to start.",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    protected void makeDiscoverable(){
+        // Make local device discoverable
+        Intent discoverableIntent = new
+                Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, DISCOVERABLE_DURATION);
+        startActivityForResult(discoverableIntent, DISCOVERABLE_BT_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        // Register the BroadcastReceiver for ACTION_FOUND
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        this.registerReceiver(broadcastReceiver, filter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        this.unregisterReceiver(broadcastReceiver);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_bluetooth, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class ListeningThread extends Thread {
+        private final BluetoothServerSocket bluetoothServerSocket;
+
+        public ListeningThread() {
+            BluetoothServerSocket temp = null;
+            try {
+                temp = bluetoothAdapter.listenUsingRfcommWithServiceRecord(getString(R.string.app_name), uuid);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            bluetoothServerSocket = temp;
+        }
+
+
+        public void run() {
+            BluetoothSocket bluetoothSocket;
+            // This will block while listening until a BluetoothSocket is returned
+            // or an exception occurs
+            while (true) {
+                try {
+                    bluetoothSocket = bluetoothServerSocket.accept();
+                } catch (IOException e) {
+                    break;
+                }
+                // If a connection is accepted
+                if (bluetoothSocket != null) {
+
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), "A connection has been accepted.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                    // Code to manage the connection in a separate thread
+                   /*
+                       manageBluetoothConnection(bluetoothSocket);
+                   */
+
+                    try {
+                        bluetoothServerSocket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+            }
+        }
+
+        // Cancel the listening socket and terminate the thread
+        public void cancel() {
+            try {
+                bluetoothServerSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class ConnectingThread extends Thread {
+        private final BluetoothSocket bluetoothSocket;
+        private final BluetoothDevice bluetoothDevice;
+
+        public ConnectingThread(BluetoothDevice device) {
+
+            BluetoothSocket temp = null;
+            bluetoothDevice = device;
+
+            // Get a BluetoothSocket to connect with the given BluetoothDevice
+            try {
+                temp = bluetoothDevice.createRfcommSocketToServiceRecord(uuid);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            bluetoothSocket = temp;
+        }
+
+        public void run() {
+
+            bluetoothAdapter.cancelDiscovery();
+
+            try {
+                bluetoothSocket.connect();
+            } catch (IOException connectException) {
+                connectException.printStackTrace();
+                try {
+                    bluetoothSocket.close();
+                } catch (IOException closeException) {
+                    closeException.printStackTrace();
+                }
+            }
+
+            // Code to manage the connection in a separate thread
+            /*
+               manageBluetoothConnection(bluetoothSocket);
+            */
+        }
+
+        // Cancel an open connection and terminate the thread
+        public void cancel() {
+            try {
+                bluetoothSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
